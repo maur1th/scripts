eb0ccd20b51b9b81262429659e52c7e9eeb485f1
Thomas Maurin <maurin.th@gmail.com>
2014-12-23 23:34:12 +0100

    nodejs tutorial

--- nodejs/time-server.js
+++ nodejs/time-server.js
@@ -1,1 +1,6 @@
-time-server.js
+var net = require('net')
+var strftime = require('strftime')
+var server = net.createServer(function (socket) {
+  socket.end(strftime("%F %R") + "\n")
+})
+server.listen(process.argv[2])
