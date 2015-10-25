4b8ce71c160f5445798e6d790380f363a24a5ee8
Thomas Maurin <maurin.th@gmail.com>
2015-10-25 13:08:35 +0100

    updated install subl script

--- install_subl.sh
+++ install_subl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo mv sublime_text_3_build* /opt
 cd /opt
+sudo rm -r sublime_text
 sudo tar xvjf sublime_text_3_build*
 sudo mv sublime_text_3 sublime_text
 sudo rm sublime_text_3_build*
